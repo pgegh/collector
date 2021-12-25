515c460338d29b5cd13e5f8e4479cfccf4addc1e
pgegh <hovig@mail.com>
2021-12-25 01:22:35 +0100

    Create current-year and parse-date auxiliary functions

--- src/collector_core/auxiliary_functions.clj
+++ src/collector_core/auxiliary_functions.clj
@@ -17,7 +17,8 @@
 
 (ns collector-core.auxiliary-functions
   (:require [clojure.test :refer [is]])
-  (:import (java.util Date)))
+  (:import (java.util Date Calendar)
+           (java.text SimpleDateFormat)))
 
 (defn now
   "Returns a Java object representing the current date"
@@ -26,3 +27,22 @@
            (is (re-find #"^#inst " (pr-str (new Date)))))}
   []
   (new Date))
+
+(defn current-year
+  "Returns the current year as Int"
+  {:test (fn []
+           (let [year (current-year)]
+             (is (and (int? year)
+                      (< 2020 year)))))}
+  []
+  (.get (Calendar/getInstance)
+        (Calendar/YEAR)))
+
+(defn parse-date
+  "Parses a date form a string"
+  {:test (fn []
+           (is (= (parse-date "yyyy-MM-dd" "2014-12-23") #inst"2014-12-22T23:00:00.000-00:00")))}
+  [pattern date-string]
+  (.parse
+    (SimpleDateFormat. pattern)
+    date-string))
