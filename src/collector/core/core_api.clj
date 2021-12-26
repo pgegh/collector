91eaea714f9591091c54fee7c60be01b6cb504fa
pgegh <hovig@mail.com>
2021-12-26 21:23:07 +0100

    Create remove-movie function

--- src/collector/core/core_api.clj
+++ src/collector/core/core_api.clj
@@ -39,7 +39,7 @@
                      "tt0000001" {:title "movie1"}}))
              (is (error? #"^A movie with the same ID exists! ID:"
                          #(-> (add-movie db mv0 "tt0000000")
-                             (add-movie mv1 "tt0000000"))))))}
+                              (add-movie mv1 "tt0000000"))))))}
   [database movie imdb-movie-id]
   {:pre  [(s/valid? :collector.core.specs/database database)
           (s/valid? :collector.core.specs/movie movie)
@@ -62,7 +62,7 @@
                   {:title "updated" :original-title "updated"}))
            (is (error? #"The movie you are trying to update does not exist in the database! Movie ID:"
                        #(-> (create-initial-database)
-                           (update-movie "tt0000000" :title "updated")))))}
+                            (update-movie "tt0000000" :title "updated")))))}
   [database imdb-movie-id & kvs]
   {:pre  [(s/valid? :collector.core.specs/database database)
           (s/valid? :collector.core.specs/imdb-movie-id imdb-movie-id)]
@@ -70,3 +70,23 @@
   (if-not (get-movie database imdb-movie-id)
     (error "The movie you are trying to update does not exist in the database! Movie ID:" imdb-movie-id)
     (update-in database [:movies-db imdb-movie-id] #(apply assoc % kvs))))
+
+(defn remove-movie
+  "Removes the given movie from the database. The movie must exist in the database.
+  The updated database will be returned"
+  {:test (fn []
+           (is (= (-> (create-initial-database)
+                      (assoc :movies-db {"tt0000000" {:title "test"}})
+                      (remove-movie "tt0000000")
+                      (get :movies-db))
+                  {}))
+           (is (error? #"^The selected movie does not exist! Movie ID:"
+                       #(-> (create-initial-database)
+                            (remove-movie "tt0000000")))))}
+  [database imdb-movie-id]
+  {:pre  [(s/valid? :collector.core.specs/database database)
+          (s/valid? :collector.core.specs/imdb-movie-id imdb-movie-id)]
+   :post [(s/valid? :collector.core.specs/database %)]}
+  (if (get-movie database imdb-movie-id)
+    (update database :movies-db #(dissoc % imdb-movie-id))
+    (error "The selected movie does not exist! Movie ID:" imdb-movie-id)))
