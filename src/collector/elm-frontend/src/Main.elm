b778ee88e9c26d8e7635ee548e7f7597cb0d1607
pgegh <hovig@mail.com>
2022-01-07 06:42:31 +0100

    Implement a loading page, a start page and an error page

--- src/collector/elm-frontend/src/Main.elm
+++ src/collector/elm-frontend/src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+import Json.Decode as JD
 
 
 main : Program () Model Msg
@@ -20,14 +22,37 @@ main =
 -- MODEL
 
 
-type alias Model =
-    { pageTitle : String
+type Model
+    = Loading
+    | StartPage StartPageData
+    | ErrorDBFileNames
+    | ErrorPage String
+
+
+type alias StartPageData =
+    { dbFileNames : FileNames
+    , selectedDBFile : String
+    , newDBFileName : String
     }
+
+
+type alias FileNames =
+    List String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model "Collector - Set Database file", Cmd.none )
+    ( Loading, loadFileNames )
+
+
+isDBFileSelected : StartPageData -> Bool
+isDBFileSelected spData =
+    case spData.selectedDBFile of
+        "" ->
+            False
+
+        _ ->
+            True
 
 
 
@@ -37,6 +62,10 @@ init _ =
 type Msg
     = Load
     | Create
+    | GetFileNames
+    | GotFileNames (Result Http.Error FileNames)
+    | UpdateSelectedDBFileName String
+    | UpdateNewDBFileName String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -47,6 +76,39 @@ update msg model =
 
         Create ->
             ( model, Cmd.none )
+
+        GetFileNames ->
+            ( Loading, loadFileNames )
+
+        GotFileNames result ->
+            case result of
+                Err _ ->
+                    ( ErrorDBFileNames, Cmd.none )
+
+                Ok dbFileNames ->
+                    ( StartPage
+                        { dbFileNames = dbFileNames
+                        , selectedDBFile = ""
+                        , newDBFileName = ""
+                        }
+                    , Cmd.none
+                    )
+
+        UpdateSelectedDBFileName fileName ->
+            case model of
+                StartPage startPageData ->
+                    ( StartPage { startPageData | selectedDBFile = fileName }, Cmd.none )
+
+                _ ->
+                    ( ErrorPage "Trying to update selected database file name when not in StartPage.", Cmd.none )
+
+        UpdateNewDBFileName fileName ->
+            case model of
+                StartPage startPageData ->
+                    ( StartPage { startPageData | newDBFileName = fileName }, Cmd.none )
+
+                _ ->
+                    ( ErrorPage "Trying to change new database file name when not in StartPage.", Cmd.none )
 
 
 
@@ -64,19 +126,80 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = model.pageTitle
-    , body =
-        [ h1 [] [ text "COLLECTOR" ]
-        , h2 [] [ text "Select a database" ]
-        , select [ name "Available databases", size 10 ]
-            --Some dummy data
-            [ option [ value "1" ] [ text "First Choice" ]
-            , option [ value "2" ] [ text "Second Choice" ]
-            , option [ value "3" ] [ text "Third Choice" ]
-            ]
-        , div []
-            [ button [ onClick Load ] [ text "Load Selected Database" ]
-            , button [ onClick Create ] [ text "Create New Database" ]
-            ]
-        ]
-    }
+    case model of
+        Loading ->
+            { title = "Collector - Loading"
+            , body = [ text "Loading..." ]
+            }
+
+        StartPage startPageData ->
+            { title = "Collector - Set Database"
+            , body =
+                [ h1 [] [ text "COLLECTOR" ]
+                , h2 [] [ text "Select a database" ]
+                , div []
+                    [ select
+                        [ name "Available databases"
+                        , size 10
+                        , on "change" (JD.map UpdateSelectedDBFileName targetValue)
+                        ]
+                        (List.map
+                            (\fileName ->
+                                option [ value fileName ] [ text fileName ]
+                            )
+                            startPageData.dbFileNames
+                        )
+                    , button
+                        [ onClick Load
+                        , disabled (not (isDBFileSelected startPageData))
+                        ]
+                        [ text "Load Selected Database" ]
+                    ]
+                , div []
+                    [ input [ type_ "text", on "change" (JD.map UpdateNewDBFileName targetValue) ] []
+                    , button [ onClick Create ] [ text "Create New Database" ]
+                    ]
+                ]
+            }
+
+        ErrorDBFileNames ->
+            { title = "Collector - Error Getting Database files"
+            , body =
+                [ h1 [] [ text "Error! Could not retrieve available database files" ]
+                , button [ onClick GetFileNames ] [ text "Retry" ]
+                ]
+            }
+
+        ErrorPage error ->
+            { title = "Collector - Error"
+            , body =
+                [ h1 [] [ text "Error!" ]
+                , p [] [ text error ]
+                ]
+            }
+
+
+
+-- HTTP
+
+
+baseUrl : String
+baseUrl =
+    "http://localhost:8001"
+
+
+loadFileNames : Cmd Msg
+loadFileNames =
+    Http.get
+        { url = baseUrl ++ "/get-available-database-files"
+        , expect = Http.expectJson GotFileNames fileNameDecoder
+        }
+
+
+
+-- JSON
+
+
+fileNameDecoder : JD.Decoder (List String)
+fileNameDecoder =
+    JD.list JD.string
