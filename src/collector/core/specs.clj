63e7c7a834873ce20b229ecfaf69ebc31c26e469
pgegh <hovig@mail.com>
2022-01-14 23:35:57 +0100

    Update core.specs to include all the entry categories

--- src/collector/core/specs.clj
+++ src/collector/core/specs.clj
@@ -23,8 +23,12 @@
 (s/def ::date #(instance? Date %))
 (s/def ::date-created ::date)
 
-(s/def ::imdb-movie-id (s/and string?
-                              #(re-find #"^tt[0-9]+$" %)))
+(s/def :imdb-movie/id (s/and string?
+                             #(re-find #"^tt[0-9]+$" %)))
+(s/def :imdb-person/id (s/and string?
+                              #(re-find #"^nm[0-9]+$" %)))
+(s/def :imdb-company/id (s/and string?
+                               #(re-find #"^co[0-9]+$" %)))
 
 (s/def :audio/id (s/and string?
                         #(re-find #"^au[0-9]{7}$" %)))
@@ -32,15 +36,22 @@
                        #(re-find #"^gm[0-9]{7}$" %)))
 (s/def :book/id (s/and string?
                        #(re-find #"^bk[0-9]{7}$" %)))
-(s/def :video/id (s/or :imdb ::imdb-movie-id
+(s/def :video/id (s/or :imdb :imdb-movie/id
                        :local (s/and string?
                                      #(re-find #"^vd[0-9]{7}$" %))))
-(s/def :company/id (s/or :imdb ::imdb-company-id
+(s/def :company/id (s/or :imdb :imdb-company/id
                          :local (s/and string?
                                        #(re-find #"^cm[0-9]{7}$" %))))
-(s/def :person/id (s/or :imdb ::imdb-person-id
+(s/def :person/id (s/or :imdb :imdb-person/id
                         :local (s/and string?
                                       #(re-find #"^pr[0-9]{7}$" %))))
+
+(s/def :collector/id (s/or :a :audio/id
+                           :b :book/id
+                           :c :company/id
+                           :d :game/id
+                           :e :person/id
+                           :f :video/id))
 
 (s/def ::name string?)
 (s/def ::birth ::date)
@@ -53,15 +64,15 @@
 (s/def ::release-date ::date)
 (s/def ::runtime-minutes (s/and int? pos?))
 (s/def ::plot string?)
-(s/def ::imdb-person-id (s/and string?
-                               #(re-find #"^nm[0-9]+$" %)))
 (s/def ::directors (s/coll-of :person/id :type set?))
 (s/def ::writers (s/coll-of :person/id :type set?))
 (s/def ::stars (s/coll-of :person/id :type set?))
 (s/def ::actors (s/coll-of :person/id :type set?))
-(s/def ::genres string?)
-(s/def ::imdb-company-id (s/and string?
-                                #(re-find #"^co[0-9]+$" %)))
+(s/def ::genres (s/coll-of ::genre :type set?))
+(s/def ::established ::date)
+(s/def ::company (s/keys :req-un [::name]
+                         :opt-un [::established
+                                  ::nationality]))
 (s/def ::companies (s/coll-of :company/id :type set?))
 (s/def ::country string?)
 (s/def ::countries (s/coll-of ::country :type set?))
@@ -69,12 +80,12 @@
 (s/def ::languages (s/coll-of ::language :type set?))
 (s/def ::nationality ::country)
 (s/def ::gender #{:male :female})
-(s/def ::established ::date)
 (s/def ::occupation string?)
-(s/def ::person (s/keys :req-un [:person/name]
+(s/def ::occupations (s/coll-of ::occupation :type set?))
+(s/def ::person (s/keys :req-un [::name]
                         :opt-un [::gender
                                  ::birth
-                                 ::occupation
+                                 ::occupations
                                  ::nationality]))
 (s/def :database/persons (s/map-of :person/id ::person))
 (s/def ::content-rating string?)
@@ -83,13 +94,64 @@
                             #(>= 10.0 %)))
 (s/def ::imdb-number-of-votes (s/and int? pos?))
 
+(s/def ::artists (s/coll-of :person/id :type set?))
+(s/def ::authors (s/coll-of :person/id :type set?))
+(s/def ::rating (s/and int? #(>= % 0) #(<= % 10)))
+(s/def ::runtime-seconds (s/and int? pos?))
+(s/def ::genre string?)
+(s/def ::album string?)
+(s/def ::franchise string?)
+
+(s/def ::audio (s/keys :req-un [::name]
+                       :opt-un [::artists
+                                ::year
+                                ::image-url
+                                ::runtime-seconds
+                                ::genre
+                                ::album
+                                ::franchise
+                                ::country
+                                ::languages
+                                ::rating]))
+(s/def :book/category string?)
+(s/def ::synopsis string?)
+(s/def :book/categories (s/coll-of :book/category :type set?))
+(s/def :book/edition (s/and int? pos?))
+(s/def ::number-of-pages (s/and int? pos?))
+(s/def ::isbn #(boolean (re-find #"^[0-9]{13}$" %)))
+
+(s/def ::publisher ::company)
+
+(s/def ::book (s/keys :req-un [::name]
+                      :opt-un [::authors
+                               ::publisher
+                               ::year
+                               :book/edition
+                               :book/categories
+                               ::franchise
+                               ::synopsis
+                               ::number-of-pages
+                               ::language
+                               ::isbn
+                               ::rating]))
+
+(s/def ::game (s/keys :req-un [::name]
+                      :opt-un [::year
+                               ::franchise
+                               ::image-url
+                               ::synopsis
+                               ::directors
+                               ::genres
+                               ::companies
+                               ::countries
+                               ::language
+                               ::rating]))
 
 (s/def ::video (s/keys :req-un [::name]
                        :opt-un [::original-title
                                 ::type
                                 ::year
                                 ::image-url
-                                ::release-date
                                 ::runtime-minutes
                                 ::plot
                                 ::directors
@@ -103,24 +165,33 @@
                                 ::content-rating
                                 ::imdb-rating
                                 ::imdb-number-of-votes]))
+
+(s/def :collector/entry (s/or :a ::audio
+                              :b ::book
+                              :c ::game
+                              :d ::video))
+
 (s/def :database/videos (s/map-of :video/id ::video))
 (s/def ::date-updated ::date)
 
 
-(s/def ::company (s/keys :req-un [::name]
-                         :opt-un [::established
-                                  ::nationality]))
-(s/def :database/companies (s/map-of :company/id ::company))
 
-(s/def :main/categories (s/keys :req-un [:database/audios
-                                         :database/books
-                                         :database/companies
-                                         :database/games
-                                         :database/videos
-                                         :database/persons
-                                         ]))
+(s/def :database/audios (s/map-of :audio/id ::audio))
+(s/def :database/books (s/map-of :book/id ::book))
+(s/def :database/companies (s/map-of :company/id ::company))
+(s/def :database/games (s/map-of :game/id ::game))
+(s/def :database/videos (s/map-of :video/id ::video))
+(s/def :database/persons (s/map-of :person/id ::person))
+
+(s/def :entry/categories (s/keys :req-un [:database/audios
+                                          :database/books
+                                          :database/companies
+                                          :database/games
+                                          :database/videos
+                                          :database/persons
+                                          ]))
 
 (s/def ::database (s/keys :req-un [::date-created
                                    ::date-updated
                                    ::file-name
-                                   :main/categories]))
+                                   :entry/categories]))
